c07a3e943b1240c12c0afe88ae4ad5a09ddbb7f3
Sudhir <101911553+sudhirJms@users.noreply.github.com>
2026-09-13 19:16:17 +0530

    Add files via upload

---
+++ public/service-worker.js
@@ -0,0 +1,1 @@
+const C="lt-v1";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["/","/index.html","/app.js","/style.css","/manifest.json","/favicon.svg"]))));self.addEventListener("fetch",e=>{if(e.request.method==="GET")e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request).catch(()=>caches.match("/"))))})
